76440a470d8f4f93066269ea248d87f051aaaa03
darren <492062702@qq.com>
2023-05-10 22:55:03 +0800

    add new

---
+++ spring-security-dynamic-url-permission/src/main/java/com/proliu/springsecuritydynamicurlpermission/SpringSecurityDynamicUrlPermissionApplication.java
@@ -0,0 +1,13 @@
+package com.proliu.springsecuritydynamicurlpermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDynamicUrlPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDynamicUrlPermissionApplication.class, args);
+	}
+
+}
